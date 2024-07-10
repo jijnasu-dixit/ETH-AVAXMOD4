ee0ded21682cc2ee3821037fd1166bb559780b34
Jijnasu Dixit <113825566+jijnasu-dixit@users.noreply.github.com>
2024-07-10 23:48:01 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
 
+contract Points is ERC20, Ownable {
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
+
+    function decimals() override public pure returns (uint8) {
+        return 0;
+    }
+
+    function getBalance() external view returns (uint256){
+        return this.balanceOf(msg.sender);
+    }
+
+    function transferTokens(address _receiver, uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
+        approve(msg.sender, _value);
+        transferFrom(msg.sender, _receiver, _value);
+    }
+
+    function burnTokens(uint _value) external {
+        require(balanceOf(msg.sender) >= _value, "You don have enough Degan tokens");
+        _burn(msg.sender, _value);
+    }
+
+    function redeem(address _from, uint _amount) external {
+        transferFrom(_from, msg.sender, _amount);
+    }
+
+    
 }
+
